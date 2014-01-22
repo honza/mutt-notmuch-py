bb7e8835cb9f78c71f3c4d5e006c3ccb3fbbf089
Honza Pokorny <me@honza.ca>
2014-01-22 15:37:44 +0100

    Add setup.py
    - #4

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+description = 'Python version of the mutt-notmuch script'
+long_desc = open('README.rst').read()
+
+setup(
+    name='mutt-notmuch-py',
+    version='1.0.0',
+    url='https://github.com/honza/mutt-notmuch-py',
+    install_requires=[],
+    description=description,
+    long_description=long_desc,
+    author='Honza Pokorny',
+    author_email='me@honza.ca',
+    maintainer='Honza Pokorny',
+    maintainer_email='me@honza.ca',
+    packages=[],
+    include_package_data=True,
+    scripts=['mutt-notmuch-py.py'],
+)
